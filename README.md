9fcd78cc4f00a833773c8ebebc7d79daef8deb4c
drunkenme <oammix@gmail.com>
2016-07-22 23:38:55 +0800

    chore: test cases of actions and README changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-[![Build Status](https://travis-ci.org/drunkenme/flow2d.svg?branch=master)](https://travis-ci.org/drunkenme/flow2d)[![Project status](https://img.shields.io/badge/status-active-brightgreen.svg)](#status)[Roadmap](https://trello.com/b/x9RSMTVk/flow2d-roadmap)
+[![Build Status](https://travis-ci.org/drunkenme/flow2d.svg?branch=master)](https://travis-ci.org/drunkenme/flow2d)[![Project status](https://img.shields.io/badge/status-active-brightgreen.svg)](#status)
+
+flow2d is an experimental purpose project, written with a minimalistic modular design philosophy, and built from the ground up to focus on cache friendly data layouts with entity-component based architecture.
+
+its far from a stable version right now, but i'll keep on making it happens. u can find our plans at [Roadmap](https://trello.com/b/x9RSMTVk/flow2d-roadmap).
---
+++ test/action_test.cpp
@@ -0,0 +1,58 @@
+#include <catch.hpp>
+#include <flow2d.hpp>
+
+USING_NS_FLOW2D;
+
+struct ActionFixture
+{
+    ActionFixture() : _world(), _manager(_world) {}
+    EntityManager   _world;
+    SystemManager   _manager;
+};
+
+TEST_CASE_METHOD(ActionFixture, "TestActionTransform")
+{
+    auto as = _manager.add<ActionSystem>();
+
+    auto object = _world.spawn();
+    auto transform = _world.add_component<Transform>(object);
+    auto executor = _world.add_component<ActionExecutor>(object);
+
+    //auto env = executor->get_environment();
+    executor->run( as->spawn<ActionMoveTo, float, const Vector2f&>(2.0f, {5.0f, 5.0f}) );
+    REQUIRE( equals(transform->get_position(), {0.f, 0.f}) );
+    REQUIRE( !executor->is_finished() );
+
+    _manager.update(1.f);
+    REQUIRE( equals(transform->get_position(), {2.5f, 2.5f}) );
+    REQUIRE( !executor->is_finished() );
+
+    _manager.update(1.0f);
+    REQUIRE( equals(transform->get_position(), {5.f, 5.f}) );
+    REQUIRE( executor->is_finished() );
+}
+
+TEST_CASE_METHOD(ActionFixture, "TestActionReuse")
+{
+    auto as = _manager.add<ActionSystem>();
+
+    for( auto iteration = 0; iteration < 32; iteration++ )
+    {
+        std::vector<Action*> alives;
+        auto factor = std::rand() % 5 + 1;
+        for( auto c = 0; c < 64; c++ )
+        {
+            auto action = as->spawn<ActionMoveTo>(2.0f, Vector2f {5.0f, 5.0f});
+
+            if( c % factor != 0 ) delete action;
+            else alives.push_back(action);
+        }
+
+        REQUIRE( as->get_size_of<ActionMoveTo>() == alives.size() );
+
+        for( auto c = 0; c < alives.size(); c++ )
+            delete alives[c];
+
+        REQUIRE( as->get_size_of<ActionMoveTo>() == 0 );
+    }
+}
